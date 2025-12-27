adee4fc4d9db0aeb572195fd526c7eb380358b10
Anjali Chaurasiya <anjalichaurasiya2348@gmail.com>
2025-12-27 22:34:11 +0530

    clicker

--- project/clicker.js
+++ project/clicker.js
@@ -4,7 +4,7 @@ body.addEventListener('click',(e)=>{
 
     const circle = document.createElement("div");
     circle.classList.add('circle');
-    circle.textContent='Merry Christmas';
+    // circle.textContent='Merry Christmas';
     const color=['#e93911ff','#33ff57','#0b1c6bff','#f333ff','#33fff5','#f3ff33','#763c2fff'];
     circle.style.backgroundColor=color[Math.floor(Math.random()*color.length)];
     circle.style.left=`${e.clientX-25}px`;
